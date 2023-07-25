cb769d81af187c2f7799254c76401580f293e049
Vladimir Lopes <69479770+vladimirlopes@users.noreply.github.com>
2023-07-25 11:45:13 -0300

    Add files via upload

--- repository/PacienteRepository.java
+++ repository/PacienteRepository.java
@@ -1,10 +1,14 @@
 package com.vlsystem.agenda.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.vlsystem.agenda.entity.Paciente;
-@Repository
+
 public interface PacienteRepository extends JpaRepository<Paciente ,Long>{
+	 Optional<Paciente>findByCpf(String cpf);
+		
+	
 
 }
